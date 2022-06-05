2238f4b912fd30a07710e64bfb79917990817991
Cartafina <camila.cartafina@kellogg.northwestern.edu>
2022-06-05 04:40:02 +0000

    lsat users stories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,12 +13,13 @@ class PostsController < ApplicationController
       @post["posted_on"] = params["post"]["posted_on"]
       @post["place_id"] = params["post"]["place_id"]
       @post["user_id"] = @current_user["id"]
+      @post.uploaded_image.attach(params["post"]["uploaded_image"])
       @post.save
       redirect_to "/places/#{@post["place_id"]}"
     else
       flash["notice"] = "Login first."
     end
-    redirect_to "/posts"
+  
   end
 
 end
